ccc1e517696f26d20ba177bc532e1835fe387c27
VIKRAM9281 <vikrambhosale928@gmail.com>
2023-11-06 17:03:19 +0530

    added file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 import { Routes,Route} from "react-router-dom" //dependence 
+import Navbar from './SubComponents/Navbar';
+import Section from './SubComponents/Section';
+import 'flowbite';
+import Slider from './SubComponents/Slider';
 
-//Components
-import Home from './Components/Home';
-import DashBoard from './Components/DashBoard';
-import Contact from './Components/Contact';
-import AboutUs from './Components/AboutUs';
-import Portfolio from './Components/Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/Dashboard' element={<DashBoard/>} />
-        <Route path='/Contact' element={<Contact/>} />
-        <Route path='/About' element={<AboutUs/>} />
-        <Route path='/Portfolio' element={<Portfolio/>} />
-      </Routes>
+      <Navbar/>
+      <Section/>
+      <Slider/>
     </div>
   );
 }
--- src/Store/Store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ResponceSlice from "../Components/ResponceSlice";
-export const Store=configureStore({
-    reducer:{
-        responceData:ResponceSlice
-    }
-})
