1f005da317f75e72fe87b7078fbbb34a5c156761
VIKRAM9281 <vikrambhosale928@gmail.com>
2023-11-07 17:27:05 +0530

    sectionimgadd

--- src/SubComponents/Section.jsx
+++ src/SubComponents/Section.jsx
@@ -3,7 +3,7 @@ import sectionimg from "../assets/img/idea-1855598_1280.jpg"
 const Section = () => {
   return (
   <>
-  <section className="bg-white dark:bg-gray-900 section1">
+  <section className="bg-white dark:bg-gray-900">
   <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
     <div className="mr-auto place-self-center lg:col-span-7">
       <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -36,7 +36,10 @@ const Section = () => {
         Speak to Sales
       </button>
     </div>
-   
+    <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+      <img src={sectionimg} alt="mockup"
+      />
+    </div>
   </div>
 </section>
 
