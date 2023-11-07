bdff575300678ce491844bb53b9ab1b14112e293
VIKRAM9281 <vikrambhosale928@gmail.com>
2023-11-07 16:19:02 +0530

    a removed

--- src/SubComponents/Navbar.jsx
+++ src/SubComponents/Navbar.jsx
@@ -15,13 +15,12 @@ const Navbar = () => {
     <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
       <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
-          <a
-            href="#"
+          <button
             className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
             aria-current="page"
           >
             Home
-          </a>
+          </button>
         </li>
         <li>
           <button
@@ -47,18 +46,18 @@ const Navbar = () => {
           </button>
         </li>
         <li>
-          <a
+          <button
             className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             Pricing
-          </a>
+          </button>
         </li>
         <li>
-          <a
+          <button
             className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
           >
             Contact
-          </a>
+          </button>
         </li>
       </ul>
     </div>
